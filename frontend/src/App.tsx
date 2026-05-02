10e6e3872b8f950ca593c8486db867d247bd47f6
ABDRRAHIM404 <abdorrahimbounga@gmail.com>
2026-05-02 23:44:22 +0100

    Working Souss Massa 360 app - ready for Vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import './index.css';
+﻿import HomePage from './pages/HomePage';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
-  );
+  return <HomePage />;
 }
 
 export default App;
